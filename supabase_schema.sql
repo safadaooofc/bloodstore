67eeaf2bd537fab8e03c0da3de77a52be8ddc76a
xsag <davigfelici@gmail.com>
2026-07-17 20:07:50 -0300

    deproyi

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -171,7 +171,7 @@ VALUES (
     {
       "id": "owner-xsag",
       "username": "xsag",
-      "password": "2368*09783@#87678923bl0d778604",
+      "password": "xsag123",
       "role": "owner",
       "name": "Dono Supremo (xsag)",
       "permissions": {
